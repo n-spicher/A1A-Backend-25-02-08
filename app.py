43137e8ac551e3aef7c00f3abe2ced3d4df6f507
Nick Spicher <nick.spicher@gmail.com>
2025-02-08 11:20:17 -0500

    Initial commit - added frontend and backend

--- app.py
+++ app.py
@@ -285,4 +285,5 @@ def chat():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port)
